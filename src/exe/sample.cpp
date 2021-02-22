955652cf167a9745c6e5e8e5fc80189658865180
E-gy <balakhelisey@gmail.com>
2021-02-22 15:42:17 +0100

    sample main use interproc

--- src/exe/sample.cpp
+++ src/exe/sample.cpp
@@ -1,7 +1,10 @@
 #include <sample.hpp>
 
+#include <util/interproc.hpp>
+
 using namespace redips;
 
-int main(){
+int main(int argc, char* args[]){
+	interproc::extNotifyReady(argc, args);
 	return mul2(0);
 }
