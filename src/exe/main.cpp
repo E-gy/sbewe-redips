9a06fb2e087ba96e35e4bbcd0de16199d046e9c0
E-gy <balakhelisey@gmail.com>
2021-02-24 23:33:34 +0100

    Look, main is a proxy now!

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -22,19 +22,50 @@ int main(int argc, char* args[]){
 				return false;
 			}, []([[maybe_unused]] const sockaddr_in& remote, const IOResource& conn){
 				conn->engine->engine <<= http::Request::read(conn) >> [=](auto rr){
-					if(auto ok = rr.ok()){
-						auto resp = http::Response();
-						resp.version = http::Version::HTTP11;
-						resp.status = http::Status::OK;
-						resp.setHeader("Content-Type", "text");
-						std::string respb = "hello world!";
-						resp.body = std::vector<char>(respb.begin(), respb.end());
-						auto wr = conn->writer();
-						resp.write(wr);
-						conn->engine->engine <<= wr->eod() >> [](auto wrr){
-							if(wrr.isOk()) std::cout << "Sent response!\n";
-							else std::cerr << "Send response error " << wrr.err() << "\n"; 
-						};
+					if(rr.isOk()){
+						std::cout << "Received request!\n";
+						http::SharedRequest reqwest = std::move(*rr.ok());
+						//Proxy go!
+						::addrinfo hints = {};
+						hints.ai_family = AF_INET;
+						hints.ai_socktype = SOCK_STREAM;
+						hints.ai_protocol = IPPROTO_TCP;
+						auto addrspaceres = NetworkedAddressInfo::find("93.184.216.34", "80", hints);
+						if(auto addrspace = addrspaceres.ok()){
+							sockaddr_in bind0 = {};
+							bind0.sin_family = AF_INET;
+							bind0.sin_addr.s_addr = INADDR_ANY;
+							bind0.sin_port = 0;
+							auto conres = netConnectTo<AF_INET, SOCK_STREAM, IPPROTO_TCP>(conn->engine, addrspace, bind0);
+							if(auto connf = conres.ok()){
+								conn->engine->engine <<= *connf >> [=](ConnectionResult connar){
+									if(connar.isOk()){
+										auto proxyconn = *connar.ok();
+										std::cout << "Proxied connection ready wooo!!!\n";
+										auto w = proxyconn->writer();
+										reqwest->write(w);
+										conn->engine->engine <<= w->eod() >> [=](auto wrr){
+											if(wrr.isOk()){
+												std::cout << "Sent proxied request!\n";
+												conn->engine->engine <<= http::Response::read(proxyconn) >> [=](auto prr){
+													if(prr.isOk()){
+														std::cout << "Received proxied response!\n";
+														auto respp = std::move(*prr.ok());
+														auto w = conn->writer();
+														respp->write(w);
+														conn->engine->engine <<= w->eod() >> [=](auto wrr){
+															if(wrr.isOk()) std::cout << "Forwarded response!\n";
+															else std::cerr << "Forward response error " << *wrr.err() << "\n";
+														};
+													} else std::cerr << "read proxied response error " << prr.err()->desc << "\n";
+												};
+											} else std::cerr << "send proxied request error " << *wrr.err() << "\n";
+										};
+									} else std::cerr << "failed to proxied connect (after await): " << *connar.err() << "\n";
+								};
+								std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+							} else std::cerr << "failed to proxied connect: " << *conres.err() << "\n";
+						} else std::cerr << "failed to get proxied address space: " << *addrspaceres.err() << "\n";
 					} else std::cerr << "Read error " << rr.err()->desc << "\n";
 				};
 				return result<void, std::string>::Ok();
