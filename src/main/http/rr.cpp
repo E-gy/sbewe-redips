7a1352a8cc963d6495122fb3f076ca9fb01b137c
E-gy <balakhelisey@gmail.com>
2021-02-24 16:54:55 +0100

    brieh

--- src/main/http/rr.cpp
+++ src/main/http/rr.cpp
@@ -5,7 +5,7 @@
 
 namespace redips::http {
 
-bool RR::hasHeader(const std::string& h){ return headers.contains(h); }
+bool RR::hasHeader(const std::string& h){ return headers.count(h) > 0; }
 bool RR::hasHeader(Header h){ return hasHeader(headerGetStr(h)); }
 std::optional<std::string> RR::getHeader(const std::string& h){ return hasHeader(h) ? std::optional(headers[h]) : std::nullopt; }
 std::optional<std::string> RR::getHeader(Header h){ return getHeader(headerGetStr(h)); }
