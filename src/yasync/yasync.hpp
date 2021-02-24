728f0c3d4892dfd1c1c9b4d92fd46ef1ce4288a3
E-gy <balakhelisey@gmail.com>
2021-02-24 15:08:21 +0100

    YAsynC superheader

---
+++ src/yasync/yasync.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "yasync/future.hpp"
+#include "yasync/engine.hpp"
+#include "yasync/impls.hpp"
+#include "yasync/iosock.hpp"
+#include "yasync/ioctrlc.hpp"
+#include "yasync/io.hpp"
