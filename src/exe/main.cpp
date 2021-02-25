3c19b8e5b4d87c3f70eb786d683accf04d5e343a
E-gy <balakhelisey@gmail.com>
2021-02-25 22:57:08 +0100

    Same main, new main
    am i abusing operators? _nooooooooo_

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -42,80 +42,64 @@ int main(int argc, char* args[]){
 		auto timeToStahpRes = CtrlC::on(&engine);
 		if(auto timeToStahp = timeToStahpRes.ok()){
 			IOYengine ioengine(&engine);
-			auto listenerr = netListen<AF_INET, SOCK_STREAM, IPPROTO_TCP, sockaddr_in>(&ioengine, []([[maybe_unused]] auto lsock, sysneterr_t err, const std::string& location){
+			netListen<AF_INET, SOCK_STREAM, IPPROTO_TCP, sockaddr_in>(&ioengine, []([[maybe_unused]] auto lsock, sysneterr_t err, const std::string& location){
 				std::cerr << "Listen error: " << printSysNetError(location, err) << "\n";
 				return false;
 			}, []([[maybe_unused]] const sockaddr_in& remote, const IOResource& conn){
 				std::cout << "conn!\n";
-				conn->engine->engine <<= http::Request::read(conn) >> [=](auto rr){
-					if(rr.isOk()){
-						std::cout << "Received request!\n";
-						http::SharedRequest reqwest = std::move(*rr.ok());
-						//Proxy go!
-						::addrinfo hints = {};
-						hints.ai_family = AF_INET;
-						hints.ai_socktype = SOCK_STREAM;
-						hints.ai_protocol = IPPROTO_TCP;
-						auto addrspaceres = NetworkedAddressInfo::find("93.184.216.34", "80", hints);
-						if(auto addrspace = addrspaceres.ok()){
-							sockaddr_in bind0 = {};
-							bind0.sin_family = AF_INET;
-							bind0.sin_addr.s_addr = INADDR_ANY;
-							bind0.sin_port = 0;
-							auto conres = netConnectTo<AF_INET, SOCK_STREAM, IPPROTO_TCP>(conn->engine, addrspace, bind0);
-							if(auto connf = conres.ok()){
-								conn->engine->engine <<= *connf >> [=](ConnectionResult connar){
-									if(connar.isOk()){
-										auto proxyconn = *connar.ok();
-										std::cout << "Proxied connection ready wooo!!!\n";
-										auto w = proxyconn->writer();
-										reqwest->write(w);
-										conn->engine->engine <<= w->eod() >> [=](auto wrr){
-											if(wrr.isOk()){
-												std::cout << "Sent proxied request!\n";
-												conn->engine->engine <<= http::Response::read(proxyconn) >> [=](auto prr){
-													if(prr.isOk()){
-														std::cout << "Received proxied response!\n";
-														auto respp = std::move(*prr.ok());
-														auto w = conn->writer();
-														respp->write(w);
-														conn->engine->engine <<= w->eod() >> [=](auto wrr){
-															if(wrr.isOk()) std::cout << "Forwarded response!\n";
-															else std::cerr << "Forward response error " << *wrr.err() << "\n";
-														};
-													} else std::cerr << "read proxied response error " << prr.err()->desc << "\n";
-												};
-											} else std::cerr << "send proxied request error " << *wrr.err() << "\n";
-										};
-									} else std::cerr << "failed to proxied connect (after await): " << *connar.err() << "\n";
-								};
-							} else std::cerr << "failed to proxied connect: " << *conres.err() << "\n";
-						} else std::cerr << "failed to get proxied address space: " << *addrspaceres.err() << "\n";
-					} else std::cerr << "Read error " << rr.err()->desc << "\n";
-				};
+				conn->engine->engine <<= http::Request::read(conn) >> ([=](http::SharedRequest reqwest){
+					std::cout << "Received request!\n";
+					//Proxy go!
+					::addrinfo hints = {};
+					hints.ai_family = AF_INET;
+					hints.ai_socktype = SOCK_STREAM;
+					hints.ai_protocol = IPPROTO_TCP;
+					NetworkedAddressInfo::find("93.184.216.34", "80", hints).ifElse([=](auto addrspace){
+						sockaddr_in bind0 = {};
+						bind0.sin_family = AF_INET;
+						bind0.sin_addr.s_addr = INADDR_ANY;
+						bind0.sin_port = 0;
+						netConnectTo<AF_INET, SOCK_STREAM, IPPROTO_TCP>(conn->engine, &addrspace, bind0).ifElse([=](auto connf){
+							conn->engine->engine <<= connf >> ([=](auto proxyconn){
+								std::cout << "Proxied connection ready wooo!!!\n";
+								auto w = proxyconn->writer();
+								reqwest->write(w);
+								conn->engine->engine <<= w->eod() >> ([=](){
+									std::cout << "Sent proxied request!\n";
+									conn->engine->engine <<= http::Response::read(proxyconn) >> ([=](auto respp){
+										std::cout << "Received proxied response!\n";
+										auto w = conn->writer();
+										respp->write(w);
+										conn->engine->engine <<= w->eod() >> ([=](){
+											std::cout << "Forwarded response!\n";
+										}|[](auto err){ std::cerr << "Forward response error " << err << "\n"; });
+									}|[](auto err){ std::cerr << "read proxied response error " << err.desc << "\n"; });
+								}|[](auto err){ std::cerr << "send proxied request error " << err << "\n"; });
+							}|[](auto err){ std::cerr << "failed to proxied connect (after await): " << err << "\n"; });
+						}, [](auto err){ std::cerr << "failed to proxied connect: " << err << "\n"; });
+					}, [](auto err){ std::cerr << "failed to get proxied address space: " << err << "\n"; });
+				}|[=](auto err){
+					std::cerr << "Read error " << err.desc << "\n";
+				});
 				return result<void, std::string>::Ok();
-			});
-			if(auto listenerp = listenerr.ok()){
-				auto listener = *listenerp;
+			}).ifElse([&](auto listener){
 				::addrinfo hints = {};
 				hints.ai_family = AF_INET;
 				hints.ai_socktype = SOCK_STREAM;
 				hints.ai_protocol = IPPROTO_TCP;
-				auto addrspaceres = NetworkedAddressInfo::find("localhost", "5050", hints);
-				if(auto addrspace = addrspaceres.ok()){
-					auto listener4 = listener->listen(addrspace);
-					if(auto listenp = listener4.ok()){
+				NetworkedAddressInfo::find("localhost", "5050", hints).ifElse([&](auto addrspace){
+					listener->listen(&addrspace).ifElse([&](auto listenp){
 						std::cout << "listening...\n";
 						interproc::extNotifyReady(argc, args);
 						engine <<= *timeToStahp >> [=]{
 							std::cout << "initating shut down\n";
 							listener->shutdown();
 						};
-						blawait(&engine, *listenp);
+						blawait(&engine, listenp);
 						std::cout << "done listening\n";
-					} else std::cerr << "failed to listen: " << *listener4.err() << "\n";
-				} else std::cerr << "failed to get address space: " << *addrspaceres.err() << "\n";
-			} else std::cerr << "failed to create listening socket: " << *listenerr.err() << "\n";
+					}, [](auto err){ std::cerr << "failed to listen: " << err << "\n"; });
+				}, [](auto err){ std::cerr << "failed to get address space: " << err << "\n"; });
+			}, [](auto err){ std::cerr << "failed to create listening socket: " << err << "\n"; });
 			CtrlC::un(&engine);
 		} else std::cerr << "failed to set up ctrl+c listener: " << *timeToStahpRes.err() << "\n";
 		std::cout << "putting an end to the work loop\n";
