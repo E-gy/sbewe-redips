109b172c85810f2201a2bcc62ab26b353622b817
E-gy <balakhelisey@gmail.com>
2021-02-27 13:27:03 +0100

    Static file server impl

---
+++ src/main/virt/statfs.cpp
@@ -0,0 +1,63 @@
+#include "statfs.hpp"
+
+#include <sys/stat.h>
+#include <fstream>
+#include <iostream>
+
+using namespace yasync;
+
+namespace redips::virt {
+
+StaticFileServer::StaticFileServer(std::string r, std::string d) : root(r), deff(d) {}
+
+constexpr auto FPATHSEP = '/';
+
+enum class FileType {
+	NE, Directory, File
+};
+
+FileType getFileType(const std::string& f){
+	struct ::stat finf;
+	if(::stat(f.c_str(), &finf) < 0) return FileType::NE;
+	return S_ISDIR(finf.st_mode) ? FileType::Directory : FileType::File;
+}
+
+void StaticFileServer::take(yasync::io::IOResource conn, redips::http::SharedRequest req){
+	auto relp = root;
+	if(relp[relp.length()-1] != FPATHSEP) relp += '/';
+	auto eop = req->path.find('?');
+	relp += eop == std::string::npos ? req->path : req->path.substr(0, eop);
+	if(relp[relp.length()-1] == '/') relp += deff;
+	else if(getFileType(relp) == FileType::Directory) relp += FPATHSEP + deff;
+	auto resp = http::SharedResponse(new http::Response());
+	auto immwr = [=](){
+		auto wr = conn->writer();
+		resp->write(wr);
+		return wr->eod() >> ([](){}|[](auto err){
+			std::cerr << "Failed to respond: " << err << "\n";
+		});
+	};
+	if(getFileType(relp) != FileType::File){
+		resp->status = http::Status::NOT_FOUND;
+		resp->setBody(std::string("File not found :("));
+		conn->engine->engine <<= immwr();
+	} else {
+		yasync::io::fileOpenRead(conn->engine, relp).ifElse([=](auto f) mutable {
+			conn->engine->engine <<= (f->template read<std::vector<char>>()) >> ([=](auto ok){
+				resp->status = http::Status::OK;
+				resp->setBody(ok);
+				return immwr();
+			}|[=](auto err){
+				resp->status = http::Status::INTERNAL_SERVER_ERROR;
+				resp->setBody(err);
+				return immwr();
+			});
+		}, [=](auto err){
+			resp->status = http::Status::INTERNAL_SERVER_ERROR;
+			resp->setBody(err);
+			conn->engine->engine <<= immwr();
+		});
+	}
+}
+
+}
---
+++ src/main/virt/statfs.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "vservr.hpp"
+
+#include <string>
+
+namespace redips::virt {
+
+class StaticFileServer : public IServer {
+	std::string root;
+	std::string deff;
+	public:
+		StaticFileServer(std::string root, std::string deff);
+		void take(yasync::io::IOResource, redips::http::SharedRequest) override;
+};
+
+}
