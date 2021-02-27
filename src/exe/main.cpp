2fbf4f9e9f5817dd1136c3eaf92cfba6856ea85d
E-gy <balakhelisey@gmail.com>
2021-02-27 17:30:28 +0100

    Main do the static file server thing!

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -4,13 +4,16 @@
 #include <http/rr.hpp>
 #include <util/interproc.hpp>
 #include <config/config.hpp>
+#include <virt/r1ot.hpp>
+#include <virt/statfs.hpp>
+#include <fiz/pservr.hpp>
 
 #include <iostream>
+#include <map>
+#include <utility>
 #include <fstream>
 
 using namespace redips;
-using namespace yasync;
-using namespace yasync::io;
 using namespace magikop;
 
 result<config::Config, std::string> parseArgs(int argc, char* args[], bool& dry){
@@ -37,61 +40,47 @@ int main(int argc, char* args[]){
 	}
 	auto config = *par.ok();
 	if(dry || config.vhosts.empty()) return 0;
-	SystemNetworkingStateControl _sysnet;
+	yasync::io::SystemNetworkingStateControl _sysnet;
 	{
-		Yengine engine(4);
-		auto timeToStahpRes = CtrlC::on(&engine);
-		if(auto timeToStahp = timeToStahpRes.ok()){
-			IOYengine ioengine(&engine);
-			netListen<AF_INET, SOCK_STREAM, IPPROTO_TCP, sockaddr_in>(&ioengine, []([[maybe_unused]] auto lsock, sysneterr_t err, const std::string& location){
-				std::cerr << "Listen error: " << printSysNetError(location, err) << "\n";
-				return false;
-			}, []([[maybe_unused]] const sockaddr_in& remote, const IOResource& conn){
-				std::cout << "conn!\n";
-				conn->engine->engine <<= http::Request::read(conn) >> ([=](http::SharedRequest reqwest){
-					std::cout << "Received request!\n";
-					//Proxy go!
-					NetworkedAddressInfo::find<AF_INET, SOCK_STREAM, IPPROTO_TCP>("93.184.216.34", "80") >> ([=](auto addrspace){
-						netConnectTo<AF_INET, SOCK_STREAM, IPPROTO_TCP, sockaddr_in>(conn->engine, &addrspace) >> ([=](auto connf){
-							conn->engine->engine <<= connf >> ([=](auto proxyconn){
-								std::cout << "Proxied connection ready wooo!!!\n";
-								auto w = proxyconn->writer();
-								reqwest->write(w);
-								conn->engine->engine <<= w->eod() >> ([=](){
-									std::cout << "Sent proxied request!\n";
-									conn->engine->engine <<= http::Response::read(proxyconn) >> ([=](auto respp){
-										std::cout << "Received proxied response!\n";
-										auto w = conn->writer();
-										respp->write(w);
-										conn->engine->engine <<= w->eod() >> ([=](){
-											std::cout << "Forwarded response!\n";
-										}|[](auto err){ std::cerr << "Forward response error " << err << "\n"; });
-									}|[](auto err){ std::cerr << "read proxied response error " << err.desc << "\n"; });
-								}|[](auto err){ std::cerr << "send proxied request error " << err << "\n"; });
-							}|[](auto err){ std::cerr << "failed to proxied connect (after await): " << err << "\n"; });
-						}|[](auto err){ std::cerr << "failed to proxied connect: " << err << "\n"; });
-					}|[](auto err){ std::cerr << "failed to get proxied address space: " << err << "\n"; });
-				}|[=](auto err){
-					std::cerr << "Read error " << err.desc << "\n";
+		yasync::Yengine engine(4);
+		auto timeToStahpRes = yasync::io::CtrlC::on(&engine);
+		if(auto err = timeToStahpRes.err()){
+			std::cerr << "failed to set up ctrl+c listener: " << *err << "\n";
+			return 1;
+		}
+		auto dun = yasync::aggregAll<void>();
+		auto timeToStahp = timeToStahpRes.ok();
+		{
+			yasync::io::IOYengine ioengine(&engine);
+			std::shared_ptr<std::list<fiz::SListener>> lists(new std::list<fiz::SListener>());
+			bool okay = true;
+			//--
+			// subject to changes!
+			//TODO cross-serve smh
+			std::map<std::pair<std::string, unsigned>, virt::R1otBuilder> terms;
+			for(auto vhost : config.vhosts) terms[{vhost.ip, vhost.port}].addService(vhost.serverName, virt::SServer(new virt::StaticFileServer(vhost.root, vhost.defaultFile.value_or("index.html"))));
+			for(auto ent : terms){
+				if(!okay) break;
+				fiz::listenOn(&ioengine, ent.first.first, ent.first.second, ent.second.build()) >> ([&](fiz::SListener li){
+					lists->push_back(li);
+					dun->add(&engine, li->onShutdown());
+				}|[&](auto err){
+					std::cerr << "Error launching listener: " << err << "\n";
+					okay = false;
 				});
-				return result<void, std::string>::Ok();
-			}) >> ([&](auto listener){
-				NetworkedAddressInfo::find<AF_INET, SOCK_STREAM, IPPROTO_TCP>("localhost", "5050") >> ([&](auto addrspace){
-					listener->listen(&addrspace) >> ([&](auto listenp){
-						std::cout << "listening...\n";
-						interproc::extNotifyReady(argc, args);
-						engine <<= *timeToStahp >> [=]{
-							std::cout << "initating shut down\n";
-							listener->shutdown();
-						};
-						blawait(&engine, listenp);
-						std::cout << "done listening\n";
-					}|[](auto err){ std::cerr << "failed to listen: " << err << "\n"; });
-				}|[](auto err){ std::cerr << "failed to get address space: " << err << "\n"; });
-			}|[](auto err){ std::cerr << "failed to create listening socket: " << err << "\n"; });
-			CtrlC::un(&engine);
-		} else std::cerr << "failed to set up ctrl+c listener: " << *timeToStahpRes.err() << "\n";
-		std::cout << "putting an end to the work loop\n";
+			}
+			//--
+			if(okay){
+				engine <<= *timeToStahp >> [lists](){
+					std::cout << "Shutting down...\n";
+					for(auto li : *lists) li->shutdown();
+				};
+				std::cout << "listening...\n";
+				interproc::extNotifyReady(argc, args);
+			} else for(auto li : *lists) li->shutdown();
+			yasync::blawait<void>(&engine, dun);
+		}
+		yasync::io::CtrlC::un(&engine);
 		engine.wle();
 	}
 	std::cout << "slaying d~~a~~emons\n";
