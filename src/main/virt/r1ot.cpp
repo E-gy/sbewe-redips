95232f50a3335280015f161d0b1b11e94902e89e
E-gy <balakhelisey@gmail.com>
2021-03-11 18:40:55 +0100

    R1ot default service

--- src/main/virt/r1ot.cpp
+++ src/main/virt/r1ot.cpp
@@ -8,11 +8,14 @@ using namespace magikop;
 
 class R1oter : public IServer {
 	std::unordered_map<std::string, SServer> services;
-	R1oter(std::unordered_map<std::string, SServer> && ss) : services(ss) {}
+	std::optional<SServer> defolt;
+	R1oter(std::unordered_map<std::string, SServer> && ss, std::optional<SServer> && d) : services(ss), defolt(d) {}
 	public:
 		friend class R1otBuilder;
 		void take(yasync::io::IOResource conn, redips::http::SharedRequest req, RespBack respb) override {
-			if(auto host = req->getHeader(http::Header::Host)) if(services.count(*host) > 0) return services[*host]->take(conn, req, respb);
+			if(auto host = req->getHeader(http::Header::Host)){
+				if(services.count(*host) > 0) return services[*host]->take(conn, req, respb);
+			} else if(defolt) return (*defolt)->take(conn, req, respb);
 			respb(http::Response(http::Status::NOT_FOUND, "No such service available"));
 		}
 };
@@ -21,6 +24,10 @@ R1otBuilder& R1otBuilder::addService(const std::string& n, SServer s){
 	services[n] = s;
 	return *this;
 }
-SServer R1otBuilder::build(){ return SServer(new R1oter(std::move(services))); }
+R1otBuilder& R1otBuilder::setDefaultService(SServer s){
+	defolt = s;
+	return *this;
+}
+SServer R1otBuilder::build(){ return SServer(new R1oter(std::move(services), std::move(defolt))); }
 
 }
--- src/main/virt/r1ot.hpp
+++ src/main/virt/r1ot.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <optional>
 
 #include "vservr.hpp"
 
@@ -9,12 +10,17 @@ namespace redips::virt {
 
 struct R1otBuilder {
 	std::unordered_map<std::string, SServer> services;
+	std::optional<SServer> defolt;
 	R1otBuilder() = default;
 	/**
 	 * Adds a service with specified name
 	 * @returns `this`
 	 */
 	R1otBuilder& addService(const std::string& name, SServer service);
+	/**
+	 * Sets given service as default
+	 */
+	R1otBuilder& setDefaultService(SServer);
 	/**
 	 * Finalizes construction of the r1oter. Useless afterwards.
 	 */
