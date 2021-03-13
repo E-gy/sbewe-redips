81a7071382d226b6462a342a326352c0786993a6
E-gy <balakhelisey@gmail.com>
2021-03-13 16:44:42 +0100

    yes i dare, also SSL print error

--- src/main/ossl/ssl.cpp
+++ src/main/ossl/ssl.cpp
@@ -2,10 +2,20 @@
 
 // #include <openssl/applink.c>
 #include <openssl/bio.h>
-#include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <sstream>
 
-namespace redips::ssl {
+namespace yasync::io::ssl {
+
+std::string printSSLError(const std::string& message, sslerr_t e){
+	std::ostringstream compose;
+	compose << message << ": ";
+	char buff[512] = {};
+	ERR_error_string_n(e, buff, sizeof(buff)-1);
+	compose << buff;
+	return compose.str();
+}
+std::string printSSLError(const std::string& message){ return printSSLError(message, ERR_get_error()); }
 
 SSLStateControl::SSLStateControl(){
 	SSL_load_error_strings();
--- src/main/ossl/ssl.hpp
+++ src/main/ossl/ssl.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
-namespace redips::ssl {
+#include <memory>
+#include <string>
+#include <util/result.hpp>
+#include <openssl/ssl.h>
+
+namespace yasync::io::ssl {
+
+using sslerr_t = unsigned long;
+std::string printSSLError(const std::string& message, sslerr_t e);
+std::string printSSLError(const std::string& message);
+template<typename R> R retSSLError(const std::string& message, sslerr_t e){ return R::Err(printSSLError(message, e)); }
+template<typename R> R retSSLError(const std::string& message){ return R::Err(printSSLError(message)); }
 
 class SSLStateControl {
 	public:
