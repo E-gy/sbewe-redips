6de90bae56ae796da450900ae6369d415fe3a0b3
E-gy <balakhelisey@gmail.com>
2021-03-17 01:25:55 +0100

    didn't help

--- src/test/yaioi.cpp
+++ src/test/yaioi.cpp
@@ -12,26 +12,28 @@ using namespace yasync;
 using namespace yasync::io;
 
 SCENARIO("ioi until EOF", "[io][yasync]"){
-	Yengine engine(1); //FIXME!
 	{
-		auto ioi = ioi2Way(&engine);
-		GIVEN("an IOI"){
-			WHEN("reading until EOF"){
-				auto red = ioi.second->read<std::string>();
-				WHEN("writing the closing the other end"){
-					{
-						auto wr = std::move(ioi.first);
-						&engine <<= wr->write<std::string>("Lorem ipsum hey wait not again!");
-					}
-					THEN("the read eventually completes with the data"){
-						auto dat = blawait(&engine, red);
-						REQUIRE(dat.isOk());
-						REQUIRE(*dat.ok() == "Lorem ipsum hey wait not again!");
+		Yengine engine(GENERATE(1)); //FIXME! more threads is inconsistent
+		{
+			auto ioi = ioi2Way(&engine);
+			GIVEN("an IOI"){
+				WHEN("reading until EOF"){
+					auto red = ioi.second->read<std::string>();
+					WHEN("writing the closing the other end"){
+						{
+							auto wr = std::move(ioi.first);
+							&engine <<= wr->write<std::string>("Lorem ipsum hey wait not again!");
+						}
+						THEN("the read eventually completes with the data"){
+							auto dat = blawait(&engine, red);
+							REQUIRE(dat.isOk());
+							REQUIRE(*dat.ok() == "Lorem ipsum hey wait not again!");
+						}
 					}
 				}
 			}
 		}
+		engine.wle();
 	}
-	engine.wle();
 	Daemons::waitForEveryoneToStop();
 }
