7ed78d99edb69f9a0f76d46efc0903972b748e1d
E-gy <balakhelisey@gmail.com>
2021-03-18 18:57:50 +0100

    Connections take care reuse

--- src/main/fiz/preqh.cpp
+++ src/main/fiz/preqh.cpp
@@ -10,8 +10,14 @@ namespace redips::fiz {
 using namespace magikop;
 using namespace http;
 
+inline bool keepAlive(SharedRequest req){
+	if(auto conn = req->getHeader(Header::Connection)) if(conn->find("close") != std::string::npos) return false;
+	return true;
+}
+
 void takeCareOfConnection(yasync::io::IOResource conn, virt::SServer vs){
 	conn->engine <<= Request::read(conn) >> ([=](SharedRequest reqwest){
+		const bool kal = keepAlive(reqwest);
 		vs->take(conn, reqwest, [=](auto resp){
 			auto wr = conn->writer();
 			{
@@ -20,9 +26,11 @@ void takeCareOfConnection(yasync::io::IOResource conn, virt::SServer vs){
 				resp.setHeader(Header::Date, std::put_time(&tm, "%a, %d %b %Y %H:%M:%S %Z"));
 			}
 			resp.setHeader(Header::Host, "Redips");
-			resp.setHeader(Header::Connection, "closed");
+			resp.setHeader(Header::Connection, kal ? "keep-alive" : "closed");
 			resp.write(wr);
-			conn->engine <<= wr->eod() >> ([](){}|[](auto err){
+			conn->engine <<= wr->eod() >> ([=](){
+				if(kal) takeCareOfConnection(conn, vs);
+			}|[](auto err){
 				std::cerr << "Failed to respond: " << err << "\n";
 			});
 		});
