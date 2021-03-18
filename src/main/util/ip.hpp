5d18a21b22522150c590e5efa8c924b7f503ec09
E-gy <balakhelisey@gmail.com>
2021-03-18 16:18:05 +0100

    IPp extended functionality

--- src/main/util/ip.hpp
+++ src/main/util/ip.hpp
@@ -11,11 +11,16 @@ inline bool isValidPort(unsigned p){ return p <= 0xFFFF; }
 struct IPp {
 	std::string ip;
 	unsigned port;
-	inline bool operator==(const IPp& other){ return ip == other.ip && port == other.port; }
+	inline std::string portstr() const noexcept { return std::to_string(port); }
+	inline bool isValidIPv4() const noexcept { return ::isValidIPv4(ip); }
+	inline bool isValidIPv6() const noexcept { return ::isValidIPv6(ip); }
+	inline bool isValidIP() const noexcept { return ::isValidIP(ip); }
+	inline bool isValidPort() const noexcept { return ::isValidPort(port); }
+	inline bool operator==(const IPp& other) const noexcept { return ip == other.ip && port == other.port; }
 };
 
 namespace std {
 	template<> struct hash<IPp> {
-		size_t operator()(const IPp& ip){ return std::hash<std::string>{}(ip.ip) ^ (ip.port << 1); }
+		size_t operator()(const IPp& ip) const noexcept { return std::hash<std::string>{}(ip.ip) ^ (ip.port << 1); }
 	};
 }
