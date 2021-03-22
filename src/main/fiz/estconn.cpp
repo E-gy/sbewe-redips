9bca811320c9d5f0bf46fde576845d5945cf4b56
E-gy <balakhelisey@gmail.com>
2021-03-22 22:46:50 +0100

    moved

--- src/main/fiz/estconn.cpp
+++ src/main/fiz/estconn.cpp
@@ -4,6 +4,20 @@ namespace redips::fiz {
 
 using namespace std::literals;
 
+template<int SDomain, int SType, int SProto, typename AddressInfo> auto createConnectionFactory(yasync::io::NetworkedAddressInfo && addr, yasync::io::IOYengine* engine, yasync::TickTack* ticktack, const yasync::TickTack::Duration& timeout){
+	return [addr = std::make_shared<yasync::io::NetworkedAddressInfo>(std::move(addr)), engine, ticktack, timeout](){
+		return yasync::io::netConnectTo<SDomain, SType, SProto, AddressInfo>(engine, addr.get()) >> ([=](auto econns){
+			auto tEC = ticktack->sleep(timeout, [=](auto, bool cancel){ if(!cancel) econns->cancel(); });
+			return econns->connest() >> [=](auto connr){
+				ticktack->stop(tEC);
+				return connr;
+			};
+		}|[](auto err){
+			return yasync::completed(yasync::io::ConnectionResult::Err(err));
+		});
+	};
+}
+
 template<int SDomain, int SType, int SProto, typename AddressInfo> result<ConnectionFactory, std::string> _findConnectionFactory(const IPp& ipp, yasync::io::IOYengine* engine, yasync::TickTack* ticktack, const yasync::TickTack::Duration& timeout){
 	auto naddr = yasync::io::NetworkedAddressInfo::find<SDomain, SType, SProto>(ipp.ip, ipp.portstr());
 	if(naddr.isOk()) return ConnectionFactory(createConnectionFactory<SDomain, SType, SProto, AddressInfo>(std::move(*naddr.ok()), engine, ticktack, timeout));
--- src/main/fiz/estconn.hpp
+++ src/main/fiz/estconn.hpp
@@ -11,23 +11,6 @@ using namespace magikop;
 
 using ConnectionFactory = std::function<yasync::Future<yasync::io::ConnectionResult>()>;
 
-/**
- * @returns () → Future<ConnectionResult>
- */
-template<int SDomain, int SType, int SProto, typename AddressInfo> auto createConnectionFactory(yasync::io::NetworkedAddressInfo && addr, yasync::io::IOYengine* engine, yasync::TickTack* ticktack, const yasync::TickTack::Duration& timeout){
-	return [addr = std::make_shared<yasync::io::NetworkedAddressInfo>(std::move(addr)), engine, ticktack, timeout](){
-		return yasync::io::netConnectTo<SDomain, SType, SProto, AddressInfo>(engine, addr.get()) >> ([=](auto econns){
-			auto tEC = ticktack->sleep(timeout, [=](auto, bool cancel){ if(!cancel) econns->cancel(); });
-			return econns->connest() >> [=](auto connr){
-				ticktack->stop(tEC);
-				return connr;
-			};
-		}|[](auto err){
-			return yasync::completed(yasync::io::ConnectionResult::Err(err));
-		});
-	};
-}
-
 result<ConnectionFactory, std::string> findConnectionFactory(const IPp& ipp, yasync::io::IOYengine* engine, yasync::TickTack* ticktack, const yasync::TickTack::Duration& timeout);
 
 }
