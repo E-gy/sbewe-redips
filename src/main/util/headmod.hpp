33a0d223ce892d222889a66cbeb442716067c1d0
E-gy <balakhelisey@gmail.com>
2021-03-22 21:49:25 +0100

    Head mod mut

--- src/main/util/headmod.hpp
+++ src/main/util/headmod.hpp
@@ -17,4 +17,16 @@ struct HeadMod {
 	/// Headers and respective values to add
 	std::unordered_map<std::string, std::string> add;
 	inline operator bool(){ return remove.size() || rename.size() || add.size(); }
+	inline HeadMod& rem(const std::string& h){
+		remove.insert(h);
+		return *this;
+	}
+	inline HeadMod& ren(const std::string& f, const std::string& t){
+		rename[f] = t;
+		return *this;
+	}
+	inline HeadMod& set(const std::string& h, const std::string& v){
+		add[h] = v;
+		return *this;
+	}
 };
