e168aa4f454c62e166a21475a0119bfc81d12ed6
E-gy <balakhelisey@gmail.com>
2021-03-22 19:09:56 +0100

    is yasync now

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -17,11 +17,6 @@
 
 namespace redips {
 
-// helper type for the visitor
-template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-// explicit deduction guide (not needed as of C++20)
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
-
 using namespace redips;
 using namespace magikop;
 
