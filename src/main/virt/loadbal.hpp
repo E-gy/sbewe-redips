c4fc3a558ad7b445ba75415b6cd0007bd812c89d
E-gy <balakhelisey@gmail.com>
2021-03-22 20:40:37 +0100

    Load bal 🔨

--- src/main/virt/loadbal.hpp
+++ src/main/virt/loadbal.hpp
@@ -4,6 +4,8 @@
 #include <random>
 #include <atomic>
 
+namespace redips::virt {
+
 /*
  * A balancer's job is to distribute <whatever> over underlying *t*hings.
  * It is effectively a no-args functor that returns a [next] thing on every call.
@@ -20,7 +22,10 @@ template<typename T> class Unbalancer {
 template<typename T> class RandomBalancer {
 	std::vector<T> ts;
 	public:
+		RandomBalancer() = default;
 		explicit RandomBalancer(const std::vector<T>& tts) : ts(tts) {}
+		void add(const T& t, unsigned w = 1){ for(unsigned i = 0; i < w; i++) ts.push_back(t); }
+		void add(T && t){ ts.push_back(std::move(t)); }
 		T operator()(){
 			static thread_local std::minstd_rand rnd;
 			return ts[rnd()%ts.size()];
@@ -31,8 +36,13 @@ template<typename T> class RoundRobinBalancer {
 	std::vector<T> ts;
 	std::atomic_uint64_t next = 0;
 	public:
+		RoundRobinBalancer() = default;
 		explicit RoundRobinBalancer(const std::vector<T>& tts) : ts(tts) {}
+		void add(const T& t, unsigned w = 1){ for(unsigned i = 0; i < w; i++) ts.push_back(t); }
+		void add(T && t){ ts.push_back(std::move(t)); }
 		T operator()(){
 			return ts[next++%ts.size()];
 		}
 };
+
+}
