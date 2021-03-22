9848c5afb97abfe5f0e051c2c203589b72d30b4a
E-gy <balakhelisey@gmail.com>
2021-03-22 21:26:13 +0100

    Helth checking load balancers

---
+++ src/main/virt/loadbalhelth.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "loadbal.hpp"
+#include <fiz/helth.hpp>
+
+namespace redips::virt {
+
+template<typename T> class FailOverBalancer {
+	fiz::HealthMonitor* helthmon;
+	std::vector<T> ts;
+	std::vector<fiz::HealthMonitor::SAH> helth;
+	bool lenient;
+	public:
+		FailOverBalancer(fiz::HealthMonitor* hmon, bool l = false) : helthmon(hmon), lenient(l) {}
+		result<void, std::string> add(T && t, const IPp& haddr, const std::string& hpa){
+			auto sah = helthmon->add(haddr, hpa);
+			if(auto err = sah.err()) return *err;
+			helth.push_back(std::move(*sah.ok()));
+			ts.push_back(std::move(t));
+			return result<void, std::string>::Ok();
+		}
+		T operator()(){
+			for(unsigned i = 0; i < helth.size(); i++) switch(*helth[i]){
+				case fiz::Health::Missing:
+					if(lenient) [[fallthrough]];
+					else break;
+				case fiz::Health::Alive:
+					return ts[i];
+				case fiz::Health::Dead:
+				default:
+					break;
+			}
+			return ts[0]; //If everyone is dead, who cares?!
+		}
+};
+
+template<typename T> class FailRobinBalancer {
+	fiz::HealthMonitor* helthmon;
+	std::vector<T> ts;
+	std::vector<fiz::HealthMonitor::SAH> helth;
+	bool lenient;
+	std::atomic_uint64_t next = 0;
+	public:
+		FailRobinBalancer(fiz::HealthMonitor* hmon, bool l = false) : helthmon(hmon), lenient(l) {}
+		result<void, std::string> add(const T& t, const IPp& haddr, const std::string& hpa, unsigned w = 1){
+			auto sah = helthmon->add(haddr, hpa);
+			if(auto err = sah.err()) return *err;
+			for(unsigned i = 0; i < w; i++){
+				helth.push_back(*sah.ok());
+				ts.push_back(t);
+			}
+			return result<void, std::string>::Ok();
+		}
+		T operator()(){
+			for(unsigned k = 0; k < helth.size(); k++){
+				unsigned i = next++%ts.size();
+				switch(*helth[i]){
+					case fiz::Health::Missing:
+						if(lenient) [[fallthrough]];
+						else break;
+					case fiz::Health::Alive:
+						return ts[i];
+					case fiz::Health::Dead:
+					default:
+						break;
+				}
+			}
+			return ts[0]; //If everyone is dead, who cares?!
+		}
+};
+
+}
