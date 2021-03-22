f187e9ae29690a0e33150ba09a690333750d6d5f
E-gy <balakhelisey@gmail.com>
2021-03-22 22:35:38 +0100

    Conn inf print

---
+++ src/main/util/conninf.cpp
@@ -0,0 +1,15 @@
+#include "conninf.hpp"
+
+#include <sstream>
+
+std::ostream& operator<<(std::ostream& os, const ConnectionInfo& conn){
+	os << "for=" << conn.address << ";proto=" << conn.protocol;
+	if(conn.host) os << ";host" << *conn.host;
+	return os << ";by=Redips";
+}
+
+std::string ConnectionInfo::to_string() const {
+	std::ostringstream os;
+	os << *this;
+	return os.str();
+}
--- src/main/util/conninf.hpp
+++ src/main/util/conninf.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <yasync/io.hpp>
+#include <ostream>
 
 struct ConnectionInfo {
 	yasync::io::IOResource connection;
 	std::string address;
 	std::string protocol;
 	std::optional<std::string> host;
+	std::string to_string() const;
 };
+std::ostream& operator<<(std::ostream&, const ConnectionInfo&);
