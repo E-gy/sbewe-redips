6a9d1e57d1c6c4244cf812519accbb741e123fcf
E-gy <balakhelisey@gmail.com>
2021-03-24 17:23:06 +0100

    Virt statFS only allow head, get, and post

--- src/main/virt/statfs.cpp
+++ src/main/virt/statfs.cpp
@@ -12,6 +12,7 @@ StaticFileServer::StaticFileServer(yasync::io::IOYengine* e, std::string r, std:
 
 void StaticFileServer::take(const ConnectionInfo&, redips::http::SharedRRaw rraw, RespBack respb){
 	rraw->as<http::Request>() >> ([&](http::Request req){
+		if(req.method != http::Method::HEAD && req.method != http::Method::GET && req.method != http::Method::POST) return respb(http::Response(http::Status::METHOD_NOT_ALLOWED));
 		auto relp = root;
 		if(relp[relp.length()-1] != FPATHSEP) relp += '/';
 		auto eop = req.path.find('?');
