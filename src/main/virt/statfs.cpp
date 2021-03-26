393fea2f66af9646d9208fed3483a37d65fd9fdf
E-gy <balakhelisey@gmail.com>
2021-03-26 23:18:14 +0100

    v statfs morrrree leniency!!!

--- src/main/virt/statfs.cpp
+++ src/main/virt/statfs.cpp
@@ -8,13 +8,12 @@ namespace redips::virt {
 
 using namespace magikop;
 
-StaticFileServer::StaticFileServer(yasync::io::IOYengine* e, std::string r, std::string d) : engine(e), root(r), deff(d) {}
+StaticFileServer::StaticFileServer(yasync::io::IOYengine* e, std::string r, std::string d) : engine(e), root(r.length() > 0 && r[r.length()-1] == FPATHSEP ? r.substr(0, r.length()-1) : r), deff(d.length() > 0 && d[0] == FPATHSEP ? d.substr(1) : d) {}
 
 void StaticFileServer::take(const ConnectionInfo&, redips::http::SharedRRaw rraw, RespBack respb){
 	rraw->as<http::Request>() >> ([&](http::Request req){
 		if(req.method != http::Method::HEAD && req.method != http::Method::GET && req.method != http::Method::POST) return respb(http::Response(http::Status::METHOD_NOT_ALLOWED));
 		auto relp = root;
-		if(relp.length() > 0 && relp[relp.length()-1] == FPATHSEP) relp = relp.substr(0, relp.length()-1); //If root ends with /, remove it as req path is required to start with /
 		relp += req.path.substr(0, std::min(req.path.find('?'), req.path.find('#')));
 		if(relp.length() > 0 && relp[relp.length()-1] == FPATHSEP) relp = relp.substr(0, relp.length()-1); //If combined path ends with /, remove it. what if it's a file?
 		if(getFileType(relp) == FileType::Directory) relp += FPATHSEP + deff; //oh so it is a directory. well then let's get default file in it!
