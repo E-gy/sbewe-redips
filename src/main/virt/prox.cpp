bb74d53ec6911edc6e2bb284e4e8e5de663e7179
E-gy <balakhelisey@gmail.com>
2021-03-27 00:16:23 +0100

    v prox oh just copy the darn thing

--- src/main/virt/prox.cpp
+++ src/main/virt/prox.cpp
@@ -5,6 +5,7 @@ namespace redips::virt {
 using namespace yasync;
 using namespace yasync::io;
 using namespace magikop;
+using namespace http;
 
 class ProxyingServer : public IServer {
 	yasync::Yengine* const engine;
@@ -12,15 +13,16 @@ class ProxyingServer : public IServer {
 	unsigned retries;
 	public:
 		ProxyingServer(yasync::Yengine* e, ProxyConnectionFactory && c, unsigned r) : engine(e), cf(std::move(c)), retries(r) {}
-		void sprr(redips::http::SharedRRaw rr, RespBack resp, unsigned tr){
+		void sprr(SharedRRaw rra, RespBack resp, unsigned tr){
 			if(tr > retries) return resp(http::Response(http::Status::BAD_GATEWAY));
 			using rDecay = result<http::SharedRRaw, http::RRReadError>;
-			const bool hhh = rr->hasHeader(http::Header::Host);
 			engine <<= cf() >> ([=](auto pair){
+				RRaw rr(*rra);
+				rr.setHeader(Header::Connection, "close");
+				if(!rr.hasHeader(Header::Host)) rr.setHeader(Header::Host, pair.second.ip);
 				auto conn = pair.first;
-				if(!hhh) rr->setHeader(http::Header::Host, pair.second.ip);
 				auto wr = conn->writer();
-				rr->write(wr);
+				rr.write(wr);
 				return wr->eod() >> ([=](){ return http::RRaw::read(conn); }|[=](auto err){
 					std::cerr << "Proxy conn fwd failed: " << err << "\n";
 					return yasync::completed(rDecay::Err(http::RRReadError("")));
@@ -32,12 +34,10 @@ class ProxyingServer : public IServer {
 			}) >> ([=](auto rrbwd){ return resp(std::move(*rrbwd)); }|[=](auto err){
 				if(err.desc == CONFAERREVERY1ISDED) return resp(http::Response(http::Status::SERVICE_UNAVAILABLE));
 				if(err.desc != "") std::cerr << "Proxy conn bwd failed: " << err.desc << "\n";
-				if(!hhh) rr->removeHeader(http::Header::Host);
-				return sprr(rr, resp, tr+1);
+				return sprr(rra, resp, tr+1);
 			});
 		}
-		void take(const ConnectionInfo& inf, redips::http::SharedRRaw rr, RespBack resp) override {
-			rr->setHeader(http::Header::Connection, "close");
+		void take(const ConnectionInfo& inf, SharedRRaw rr, RespBack resp) override {
 			auto fwd = rr->getHeader(http::Header::Forwarded);
 			rr->setHeader(http::Header::Forwarded, fwd.has_value() && *fwd != "" ? *fwd + "," + inf.to_string() : inf.to_string());
 			return sprr(rr, resp, 0);
@@ -51,15 +51,16 @@ class PooledProxyingServer : public IServer {
 	unsigned retries;
 	public:
 		PooledProxyingServer(yasync::Yengine* e, ProxyConnectionFactory && c, ProxyConnectionUnfactory && u, unsigned r) : engine(e), cf(std::move(c)), uf(std::move(u)), retries(r) {}
-		void sprr(redips::http::SharedRRaw rr, RespBack resp, unsigned tr){
+		void sprr(SharedRRaw rra, RespBack resp, unsigned tr){
 			if(tr > retries) return resp(http::Response(http::Status::BAD_GATEWAY));
 			using rDecay = result<http::SharedRRaw, http::RRReadError>;
-			const bool hhh = rr->hasHeader(http::Header::Host);
 			engine <<= cf() >> ([=](auto pair){
+				RRaw rr(*rra);
+				rr.setHeader(Header::Connection, "keep-alive");
+				if(!rr.hasHeader(Header::Host)) rr.setHeader(Header::Host, pair.second.ip);
 				auto conn = pair.first;
-				if(!hhh) rr->setHeader(http::Header::Host, pair.second.ip);
 				auto wr = conn->writer();
-				rr->write(wr);
+				rr.write(wr);
 				engine <<= wr->eod() >> ([=](){ return http::RRaw::read(conn); }|[=](auto err){
 					return yasync::completed(rDecay::Err(http::RRReadError(err)));
 				}) >> ([=](auto rrbwd){
@@ -68,18 +69,15 @@ class PooledProxyingServer : public IServer {
 				}|[=](auto err){
 					std::cerr << "Proxy rw failed: " << err.desc << "\n";
 					uf(ConnectionResult::Err(err.desc));
-					if(!hhh) rr->removeHeader(http::Header::Host);
-					return sprr(rr, resp, tr+1);
+					return sprr(rra, resp, tr+1);
 				});
 			}|[=](auto err){
 				if(err == CONFAERREVERY1ISDED) return resp(http::Response(http::Status::SERVICE_UNAVAILABLE));
 				std::cerr << "Proxy est conn failed: " << err << "\n";
-				if(!hhh) rr->removeHeader(http::Header::Host);
-				return sprr(rr, resp, tr+1);
+				return sprr(rra, resp, tr+1);
 			});
 		}
-		void take(const ConnectionInfo& inf, redips::http::SharedRRaw rr, RespBack resp) override {
-			rr->setHeader(http::Header::Connection, "keep-alive");
+		void take(const ConnectionInfo& inf, SharedRRaw rr, RespBack resp) override {
 			auto fwd = rr->getHeader(http::Header::Forwarded);
 			rr->setHeader(http::Header::Forwarded, fwd.has_value() && *fwd != "" ? *fwd + "," + inf.to_string() : inf.to_string());
 			return sprr(rr, resp, 0);
