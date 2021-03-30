ca4f249b0fc547b9c467407bee784b22b3786698
E-gy <balakhelisey@gmail.com>
2021-03-30 19:47:47 +0200

    v statfs generate default file where missing

--- src/main/virt/statfs.hpp
+++ src/main/virt/statfs.hpp
@@ -10,8 +10,9 @@ class StaticFileServer : public IServer {
 	yasync::io::IOYengine* const engine;
 	std::string root;
 	std::string deff;
+	bool gmd;
 	public:
-		StaticFileServer(yasync::io::IOYengine*, std::string root, std::string deff);
+		StaticFileServer(yasync::io::IOYengine*, std::string root, std::string deff, bool gmd);
 		void take(const ConnectionInfo&, redips::http::SharedRRaw, RespBack) override;
 };
 
