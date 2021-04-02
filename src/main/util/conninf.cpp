e0c93d79d0c38d2596ea564b04b038194eb00a7e
E-gy <balakhelisey@gmail.com>
2021-04-02 17:49:20 +0200

    conn inf to string safeguard IPv6 for

--- src/main/util/conninf.cpp
+++ src/main/util/conninf.cpp
@@ -1,9 +1,12 @@
 #include "conninf.hpp"
 
 #include <sstream>
+#include "ip.hpp"
 
 std::ostream& operator<<(std::ostream& os, const ConnectionInfo& conn){
-	os << "for=" << conn.address;
+	os << "for=";
+	if(isValidIPv6(conn.address)) os << "\"[" << conn.address << "]\"";
+	else os << conn.address;
 	if(conn.host) os << ";host=" << *conn.host;
 	return os << ";proto=" << conn.protocol;
 }
